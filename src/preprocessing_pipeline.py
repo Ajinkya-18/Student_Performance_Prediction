671938364ae08adacd598d204f9b7ceeae25ecb9
Ajinkya Tamhankar <ajinkya.tamhankar18@gmail.com>
2025-06-30 01:49:02 +0530

    Changes made to model pipeline in notebook.

--- src/preprocessing_pipeline.py
+++ src/preprocessing_pipeline.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 
-from utils import load_data, clean_data
+from utils import load_data, clean_data, split_data
 
 df_path = 'data/math_subject_grade_prediction.csv'
 
@@ -14,21 +14,28 @@ df = load_data(df_path)
 
 df = clean_data(df)
 
+print(f"DataFrame Shape: {df.shape}")
+
 
 onehot_cols = ['Father_Job', 'Mother_Job']
 lab_enc_cols = ['School_Support', 'Higher_Edu', 'Extra_Paid_Classes']
 scaling_cols = ['Past_Grade_Record', 'School_Absences', 'Past_Class_Failure_Count', 
-                'Family_Relationship', 'Goes_Out', 'Mother_Job', 'Weekly_Study_Time', 
-                'Alcohol_Consumption', 'Father_Job', 'Freetime_After_School', 'Age', 
-                'Parents_Education']
+                'Family_Relationship', 'Goes_Out', 'Weekly_Study_Time', 'Age',
+                'Alcohol_Consumption', 'Freetime_After_School', 'Parents_Education']
 
+one_hot_enc = OneHotEncoder()
+lab_enc = LabelEncoder()
+rob_scaler = RobustScaler()
 
-transformers = [('onehot_enc', OneHotEncoder(), onehot_cols),
-                ('lab_enc', LabelEncoder(), lab_enc_cols),
-                ('scaler', RobustScaler(), scaling_cols)]
+one_hot_encoded_features = one_hot_enc.get_feature_names_out(onehot_cols)
+
+transformers = [('onehot_enc', one_hot_enc, onehot_cols),
+                ('lab_enc', lab_enc, lab_enc_cols),
+                ('scaler', rob_scaler, scaling_cols)]
 col_trnsfrmr = ColumnTransformer(transformers=transformers, verbose=True, n_jobs=6)
+col_trnsfrmr.fit
 
+# steps = [()]
+# pipeline = Pipeline(steps=steps, verbose=True)
 
-steps = [(), ('col_trnsfrmr', col_trnsfrmr), ()]
-pipeline = Pipeline(steps=steps, verbose=True)
-
+x_train, x_test, y_train, y_test = split_data(df)
--- src/train.py
+++ src/train.py
@@ -10,4 +10,15 @@ svr = LinearSVR(random_state=42, verbose=1)
 knr = KNeighborsRegressor(n_neighbors=10, n_jobs=6, weights='distance')
 rfr = RandomForestRegressor(random_state=42, n_estimators=300, n_jobs=6, oob_score=True)
 
+def train_model(x_train, y_train, model=lr):
+    model.fit(x_train, y_train)
+    print(f"Model Score: {model.score(x_train, y_train)}")
 
+    return model
+
+def test_model(x_test, y_test, model=lr):
+    y_preds = model.predict(x_test)
+    
+    from sklearn.metrics import r2_score
+    
+    return r2_score(y_test, y_preds)
